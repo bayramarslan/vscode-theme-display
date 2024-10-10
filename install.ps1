2e6b55f2f25aba562fa7bb3770504beb8bb47cdc
Bayram Arslan <bayramweb@gmail.com>
2024-10-10 15:57:30 +0300

    chore

--- install.ps1
+++
@@ -1,7 +0,0 @@
-$latestVersion = Get-ChildItem -Path $extensionFolder -Filter "theme-display-*.vsix" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-
-if ($latestVersion -ne $null) {
-    code --install-extension $latestVersion.FullName
-} else {
-    Write-Output "File not found."
-}
